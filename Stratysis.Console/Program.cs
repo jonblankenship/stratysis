ce5a8a65d82a72c64ecc640bfecf5f709dfe4d6a
Jon Blankenship <jon@jonblankenship.com>
2019-04-06 15:57:48 -0500

    Switch to file provider

--- Stratysis.Console/Program.cs
+++ Stratysis.Console/Program.cs
@@ -47,7 +47,7 @@ namespace Stratysis.Console
                 {
                     Symbol = "MSFT"
                 },
-                DataProviderType = DataProviderTypes.QuandlWeb
+                DataProviderType = DataProviderTypes.QuandlFile
             };
 
             var strategy = new SimpleBreakoutStrategy(20, 10);
